909098412020f1d862e2e595b2f4220d8bd55a9c
Ali <ali.gharaei360@gmail.com>
2025-07-20 16:13:48 +0330

    fix(routes): add prices route.

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\V1\TourController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/tours')->group(function () {
+    Route::get('/prices', [TourController::class, 'prices']);
     Route::get('/', [TourController::class, 'index']);
     Route::get('/{id}', [TourController::class, 'show']);
     Route::get('/{id}/availability', [TourController::class, 'availability']);
